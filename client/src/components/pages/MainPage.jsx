9dfc7f4a8bd2a7b583edb099043d0aa22a7201da
wastor3332 <serebrinv@gmail.com>
2024-08-22 15:49:10 +0300

    Ruchka comm + reit

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -5,13 +5,8 @@ export default function MainPage({user}) {
   const { routes } = useRoutes();
   return (
     <div>
-<<<<<<< HEAD
-      {routes.map((el) => 
-      <RouteCard user={user} key={el.id} route={el}/>
-=======
       {routes.map((route) => 
       <RouteCard key={route.id} route={route} user={user}/>
->>>>>>> dev
       )}
     </div>
   )
--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -54,6 +54,6 @@ export default function useUser() {
     user,
     logoutHandler,
     signUpHandler,
-    loginHandler
-  }
+    loginHandler,
+  };
 }
