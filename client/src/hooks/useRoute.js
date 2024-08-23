4e30c037447ac80e7248866e30c68fd8b8c0b871
Komrakova <baby-sale@mail.ru>
2024-08-23 12:02:52 +0300

    v6

--- client/src/hooks/useRoute.js
+++ client/src/hooks/useRoute.js
@@ -2,20 +2,17 @@ import { useEffect, useState } from "react";
 import axiosInstance from "../components/api/axiosInstance";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function useRoute() {
+export default function useRoute(user) {
   const [route, setRoute] = useState([]);
   const { id } = useParams();
-  const navigate = useNavigate();
+  
 
   useEffect(() => {
     axiosInstance(`/routers/${id}`).then(({ data }) => {
       setRoute(data);
     });
-  }, []);
-  const deleteHandler = async () => {
-    await axiosInstance.delete(`/routers/${route.id}`);
-    navigate("/routers");
-  };
+  }, [id]);
+
   return {
     route,
   };
